84916338af1989d3ab692b68ec852ea7a0c7d8cc
Andre Luis <meiahora7203@gmail.com>
2023-03-31 15:51:05 -0300

    line chart

--- src/cliente.js
+++ src/cliente.js
@@ -1,25 +1,32 @@
-import React, { useEffect, useState } from 'react';
 import io from 'socket.io-client';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { useEffect, useState } from 'react';
 import {
+  BarChart,
+  Bar,
+  Line,
   LineChart,
   XAxis,
   YAxis,
-  Line,
+  Tooltip
 } from 'recharts';
 
 const socket = io('http://localhost:3000', {
   transports: ['websocket', 'polling']
 });
 
-const RealTimeCpuUsage = () => {
+const App = ({}) => {
   const [data, setData] = useState([]);
 
+  // 1. listen for a cpu event and update the state
   useEffect(() => {
     socket.on('cpu', cpuPercent => {
       setData(currentData => [...currentData, cpuPercent]);
     });
   }, []);
 
+  // 2. render the line chart using the state
   return (
     <div>
       <h1>Real Time CPU Usage</h1>
@@ -32,12 +39,4 @@ const RealTimeCpuUsage = () => {
   );
 };
 
-const App = () => {
-  return (
-    <div>
-      <RealTimeCpuUsage />
-    </div>
-  );
-};
-
-export default App;
+ReactDOM.render(<App />, document.getElementById('root'));
